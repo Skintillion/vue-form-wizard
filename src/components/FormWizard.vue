5ee86694d17c83075aec130169c5db1de87253aa
Jeremy Jackson <jeremy@buzzit.ca>
2020-04-06 18:13:54 -0700

    Math again

--- src/components/FormWizard.vue
+++ src/components/FormWizard.vue
@@ -221,7 +221,7 @@
       },
       lastStepPercentage () {
         let stepMultiplier = 2
-        return this.stepPercentage * ((this.tabCount * stepMultiplier) - 1);
+        return this.stepPercentage * (this.tabCount -1) * stepMultiplier);
       },
       progressBarBackStyle () {
         return {
